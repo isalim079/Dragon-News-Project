a19e5ad0eee82254e38ff11d3711942579534d63
isalim079 <salimreza1025@gmail.com>
2024-01-16 20:52:20 +0600

    grid added in home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,23 +1,18 @@
 import { Box, Grid } from "@mui/material";
 
-
 export default function Home() {
-  return (
-    <div>
- 
-    <Box sx={{ flexGrow: 1 }}>
-      <Grid container spacing={2}>
-        <Grid item xs={8}>
-         
-        </Grid>
-        <Grid item xs={4}>
-        
-        </Grid>
-       
-      </Grid>
-    </Box>
-
-
-    </div>
-  )
+    return (
+        <div>
+            <Box sx={{ flexGrow: 1 }}>
+                <Grid container spacing={2}>
+                    <Grid item xs={8}>
+                        <h1>Latest News</h1>
+                    </Grid>
+                    <Grid item xs={4}>
+                        <h1>SideBar</h1>
+                    </Grid>
+                </Grid>
+            </Box>
+        </div>
+    );
 }
